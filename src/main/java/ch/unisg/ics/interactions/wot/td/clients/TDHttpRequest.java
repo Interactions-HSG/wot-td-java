98dccd4571bc41985ff308eea7e8bb3bd429f83f
daniv <ece7999@upnet.gr>
2020-06-24 18:18:50 +0200

    Fix illegal argument exception messages

--- src/main/java/ch/unisg/ics/interactions/wot/td/clients/TDHttpRequest.java
+++ src/main/java/ch/unisg/ics/interactions/wot/td/clients/TDHttpRequest.java
@@ -28,139 +28,139 @@ import ch.unisg.ics.interactions.wot.td.schemas.ObjectSchema;
 public class TDHttpRequest {
   private final Form form;
   private BasicClassicHttpRequest request;
-  
+
   public TDHttpRequest(Form form, String operationType) {
     this.form = form;
-    
+
     Optional<String> methodName = form.getMethodName(operationType);
-    
+
     if (methodName.isPresent()) {
       this.request = new BasicClassicHttpRequest(methodName.get(), form.getTarget());
     } else {
-      throw new IllegalArgumentException("No default binding for the given operation type: " 
+      throw new IllegalArgumentException("No default binding for the given operation type: "
           + operationType);
     }
-    
+
     this.request.setHeader(HttpHeaders.CONTENT_TYPE, form.getContentType());
   }
-  
+
   public int execute() {
     HttpClient client = HttpClients.createDefault();
-    
+
     try {
       return client.execute(request).getCode();
     } catch (IOException e) {
       e.printStackTrace();
     }
-    
+
     return HttpStatus.SC_BAD_REQUEST;
   }
-  
-//  static HttpRequest buildHttpRequest(Form form, Optional<DataSchema> uriSchema, 
-//      Map<String, Object> uriVariables, Optional<DataSchema> payloadSchema, 
+
+//  static HttpRequest buildHttpRequest(Form form, Optional<DataSchema> uriSchema,
+//      Map<String, Object> uriVariables, Optional<DataSchema> payloadSchema,
 //      Map<String, Object> payload) {
 //    // TODO: add query parameters
 //    SimpleHttpRequest request = SimpleHttpRequests.create(form.getMethodName(), form.getHref());
-//    
+//
 //    if (payloadSchema.isPresent() && !payload.isEmpty()) {
 //      DataSchema schema = payloadSchema.get();
-//      
+//
 //      if (schema instanceof ObjectSchema) {
 //        Map<String, Object> instance = ((ObjectSchema) schema).instantiate(payload);
-//        
+//
 //        String body = new Gson().toJson(instance);
-//        
+//
 //        request.setBody(body, ContentType.create(form.getContentType()));
 //        request.setHeader(HttpHeaders.CONTENT_TYPE, form.getContentType());
 //      }
 //    }
-//    
+//
 //    return request;
 //  }
-  
-  public TDHttpRequest setPrimitivePayload(DataSchema dataSchema, boolean value) 
+
+  public TDHttpRequest setPrimitivePayload(DataSchema dataSchema, boolean value)
       throws IllegalArgumentException {
     if (dataSchema.getDatatype() == DataSchema.BOOLEAN) {
-      request.setEntity(new StringEntity(String.valueOf(value), 
+      request.setEntity(new StringEntity(String.valueOf(value),
           ContentType.create(form.getContentType())));
     } else {
-      throw new IllegalArgumentException("The expected datatype is not a boolean value (datatype: " 
+      throw new IllegalArgumentException("The expected datatype is not a boolean value (datatype: "
           + dataSchema.getDatatype());
     }
-    
+
     return this;
   }
-  
-  public TDHttpRequest setPrimitivePayload(DataSchema dataSchema, String value) 
+
+  public TDHttpRequest setPrimitivePayload(DataSchema dataSchema, String value)
       throws IllegalArgumentException {
     if (dataSchema.getDatatype() == DataSchema.STRING) {
       request.setEntity(new StringEntity(value, ContentType.create(form.getContentType())));
     } else {
-      throw new IllegalArgumentException("The expected datatype is not a boolean value (datatype: " 
+      throw new IllegalArgumentException("The expected datatype is not a string value (datatype: "
           + dataSchema.getDatatype());
     }
-    
+
     return this;
   }
-  
-  public TDHttpRequest setPrimitivePayload(DataSchema dataSchema, long value) 
+
+  public TDHttpRequest setPrimitivePayload(DataSchema dataSchema, long value)
       throws IllegalArgumentException {
     if (dataSchema.getDatatype() == DataSchema.INTEGER) {
-      request.setEntity(new StringEntity(String.valueOf(value), 
+      request.setEntity(new StringEntity(String.valueOf(value),
           ContentType.create(form.getContentType())));
     } else {
-      throw new IllegalArgumentException("The expected datatype is not a boolean value (datatype: " 
+      throw new IllegalArgumentException("The expected datatype is not an integer value (datatype: "
           + dataSchema.getDatatype());
     }
-    
+
     return this;
   }
-  
-  public TDHttpRequest setPrimitivePayload(DataSchema dataSchema, double value) 
+
+  public TDHttpRequest setPrimitivePayload(DataSchema dataSchema, double value)
       throws IllegalArgumentException {
     if (dataSchema.getDatatype() == DataSchema.NUMBER) {
-      request.setEntity(new StringEntity(String.valueOf(value), 
+      request.setEntity(new StringEntity(String.valueOf(value),
           ContentType.create(form.getContentType())));
     } else {
-      throw new IllegalArgumentException("The expected datatype is not a boolean value (datatype: " 
+      throw new IllegalArgumentException("The expected datatype is not a number value (datatype: "
           + dataSchema.getDatatype());
     }
-    
+
     return this;
   }
-  
+
   public TDHttpRequest setObjectPayload(ObjectSchema objectSchema, Map<String, Object> payload) {
     if (objectSchema.validate(payload)) {
       Map<String, Object> instance = objectSchema.instantiate(payload);
       String body = new Gson().toJson(instance);
       request.setEntity(new StringEntity(body, ContentType.create(form.getContentType())));
     }
-    
+
     return this;
   }
-  
+
   public TDHttpRequest setArrayPayload(ArraySchema arraySchema, List<Object> payload) {
     if (arraySchema.validate(payload)) {
       String body = new Gson().toJson(payload);
       request.setEntity(new StringEntity(body, ContentType.create(form.getContentType())));
     }
-    
+
     return this;
   }
-  
+
   BasicClassicHttpRequest getRequest() {
     return this.request;
   }
-  
+
   @Override
   public String toString() {
     StringBuilder builder = new StringBuilder();
     builder.append("[TDHttpRequest] Method: " + request.getMethod());
-    
+
     try {
       builder.append(", Target: " + request.getUri().toString());
       builder.append(", Content-Type: " + request.getHeader(HttpHeaders.CONTENT_TYPE).getValue());
-      
+
       if (request.getEntity() != null) {
         StringWriter writer = new StringWriter();
         IOUtils.copy(request.getEntity().getContent(), writer, StandardCharsets.UTF_8.name());
@@ -169,8 +169,8 @@ public class TDHttpRequest {
     } catch (UnsupportedOperationException | IOException | URISyntaxException | ProtocolException e) {
       e.printStackTrace();
     }
-    
+
     return builder.toString();
   }
-  
+
 }
