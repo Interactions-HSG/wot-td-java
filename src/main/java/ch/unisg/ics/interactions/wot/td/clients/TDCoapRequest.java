f45a42f5817ab2eabd086b503093ea3bb2e61cb1
danaivach <danai.vacht@gmail.com>
2021-06-21 19:20:12 +0200

    Add coap client request

---
+++ src/main/java/ch/unisg/ics/interactions/wot/td/clients/TDCoapRequest.java
@@ -0,0 +1,177 @@
+package ch.unisg.ics.interactions.wot.td.clients;
+
+import ch.unisg.ics.interactions.wot.td.affordances.Form;
+import ch.unisg.ics.interactions.wot.td.schemas.ArraySchema;
+import ch.unisg.ics.interactions.wot.td.schemas.DataSchema;
+import ch.unisg.ics.interactions.wot.td.schemas.ObjectSchema;
+import com.google.gson.Gson;
+import org.apache.hc.core5.http.ParseException;
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.coap.Request;
+import org.eclipse.californium.elements.exception.ConnectorException;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TDCoapRequest {
+  private final static Logger LOGGER = Logger.getLogger(TDHttpRequest.class.getCanonicalName());
+
+  private final Form form;
+  private Request request;
+
+  public TDCoapRequest(Form form, String operationType) {
+    this.form = form;
+
+    Optional<String> methodName = form.getMethodName(operationType);
+
+    if (methodName.isPresent()) {
+      this.request = new Request(CoAP.Code.valueOf(methodName.get()));
+      this.request.setURI(form.getTarget());
+    } else {
+      throw new IllegalArgumentException("No default binding for the given operation type: "
+        + operationType);
+    }
+
+    this.request.getOptions().setContentFormat(MediaTypeRegistry.parse(form.getContentType()));
+  }
+
+  public TDCoapResponse execute() throws IOException, ConnectorException {
+    CoapClient client = new CoapClient();
+    CoapResponse response = client.advanced(request);
+    return new TDCoapResponse((CoapResponse) response);
+  }
+
+  public TDCoapRequest addOption(String key, String value) {
+    // TODO Support CoAP options e.g. for observation flag
+    return null;
+  }
+
+  public TDCoapRequest setPrimitivePayload(DataSchema dataSchema, boolean value)
+    throws IllegalArgumentException {
+    if (dataSchema.getDatatype().equals(DataSchema.BOOLEAN)) {
+      request.setPayload(String.valueOf(value));
+    } else {
+      throw new IllegalArgumentException("The payload's datatype does not match BooleanSchema "
+        + "(payload datatype: " + dataSchema.getDatatype() + ")");
+    }
+
+    return this;
+  }
+
+  public TDCoapRequest setPrimitivePayload(DataSchema dataSchema, String value)
+    throws IllegalArgumentException {
+    if (dataSchema.getDatatype().equals(DataSchema.STRING)) {
+      request.setPayload(String.valueOf(value));
+    } else {
+      throw new IllegalArgumentException("The payload's datatype does not match StringSchema "
+        + "(payload datatype: " + dataSchema.getDatatype() + ")");
+    }
+
+    return this;
+  }
+
+  public TDCoapRequest setPrimitivePayload(DataSchema dataSchema, long value)
+    throws IllegalArgumentException {
+    if (dataSchema.getDatatype().equals(DataSchema.INTEGER)
+      || dataSchema.getDatatype().equals(DataSchema.NUMBER)) {
+      request.setPayload(String.valueOf(value));
+    } else {
+      throw new IllegalArgumentException("The payload's datatype does not match IntegerSchema or "
+        + "NumberSchema (payload datatype: " + dataSchema.getDatatype() + ")");
+    }
+
+    return this;
+  }
+
+  public TDCoapRequest setPrimitivePayload(DataSchema dataSchema, double value)
+    throws IllegalArgumentException {
+    if (dataSchema.getDatatype().equals(DataSchema.NUMBER)) {
+      request.setPayload(String.valueOf(value));
+    } else {
+      throw new IllegalArgumentException("The payload's datatype does not match NumberSchema "
+        + "(payload datatype: " + dataSchema.getDatatype() + ")");
+    }
+
+    return this;
+  }
+
+  /**
+   * Sets a payload of type <code>ObjectSchema</code>. The object payload is given as a map where:
+   * <ul>
+   * <li>a key is a string that represents either a semantic type or an object property name</li>
+   * <li>a value can be a primitive, an object represented as a <code>Map&lt;String,Object&gt;</code>
+   * (that is, a nested object), or an ordered list of values of type <code>List&lt;Object&gt;</code></li>
+   * </ul>
+   *
+   * @param objectSchema schema to be used for validating the payload and constructing the body of
+   *                     the request
+   * @param payload      the actual payload
+   * @return this <code>TDCoapRequest</code>
+   */
+  public TDCoapRequest setObjectPayload(ObjectSchema objectSchema, Map<String, Object> payload) {
+    if (objectSchema.validate(payload)) {
+      Map<String, Object> instance = objectSchema.instantiate(payload);
+      String body = new Gson().toJson(instance);
+      request.setPayload(body);
+    }
+
+    return this;
+  }
+
+  /**
+   * Sets a payload of type <code>ArraySchema</code>. The payload is given as an ordered list of
+   * values of type <code>List&lt;Object&gt;</code>. Values can be primitives, objects represented
+   * as <code>Map&lt;String,Object&gt;</code>, or lists of values (that is, nested lists).
+   *
+   * @param arraySchema schema used for validating the payload and constructing the body of
+   *                    the request
+   * @param payload     the actual payload
+   * @return this <code>TDHttpRequest</code>
+   */
+  public TDCoapRequest setArrayPayload(ArraySchema arraySchema, List<Object> payload) {
+    if (arraySchema.validate(payload)) {
+      String body = new Gson().toJson(payload);
+      request.setPayload(body);
+    }
+
+    return this;
+  }
+
+  public String getPayloadAsString() throws ParseException, IOException {
+    return request.getPayloadString();
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("[TDCoapRequest] Method: " + request.getCode().name());
+
+    try {
+      builder.append(", Target: " + request.getURI());
+
+
+      builder.append(", " + request.getOptions().toString());
+
+
+      if (request.getPayload() != null) {
+        builder.append(", Payload: " + request.getPayloadString());
+      }
+    } catch (UnsupportedOperationException e) {
+      LOGGER.log(Level.WARNING, e.getMessage());
+    }
+
+    return builder.toString();
+  }
+
+  Request getRequest() {
+    return this.request;
+  }
+
+}
