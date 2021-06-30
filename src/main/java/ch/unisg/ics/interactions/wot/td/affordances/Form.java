580d609b7f4a34dec7e3c2e641bf5cf7181b4f06
danaivach <danai.vacht@gmail.com>
2021-06-30 23:08:08 +0200

    Add CoAP default bindings

--- src/main/java/ch/unisg/ics/interactions/wot/td/affordances/Form.java
+++ src/main/java/ch/unisg/ics/interactions/wot/td/affordances/Form.java
@@ -1,30 +1,34 @@
 package ch.unisg.ics.interactions.wot.td.affordances;
 
+import ch.unisg.ics.interactions.wot.td.vocabularies.COV;
+import ch.unisg.ics.interactions.wot.td.vocabularies.TD;
+import org.apache.commons.collections.map.MultiKeyMap;
+
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import ch.unisg.ics.interactions.wot.td.vocabularies.TD;
 
 public class Form {
-  private static final Map<String, String> DEFAULT_HTTP_BINDING = Stream.of(new String[][] { 
-    { TD.readProperty, "GET" }, 
-    { TD.writeProperty, "PUT" },
-    { TD.invokeAction, "POST" },
-  }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
-  
-  private Optional<String> methodName;
+
+  private static final MultiKeyMap DEFAULT_BINDING = new MultiKeyMap();
+  static {
+    DEFAULT_BINDING.put("HTTP", TD.readProperty, "GET");
+    DEFAULT_BINDING.put("HTTP", TD.writeProperty, "PUT");
+    DEFAULT_BINDING.put("HTTP", TD.invokeAction, "POST");
+    DEFAULT_BINDING.put("COAP", TD.readProperty, "GET");
+    DEFAULT_BINDING.put("COAP", TD.writeProperty, "PUT");
+    DEFAULT_BINDING.put("COAP", TD.invokeAction, "POST");
+    DEFAULT_BINDING.put("COAP", TD.observeProperty, "POST");
+  }
+
   private final String target;
   private final String contentType;
   private final Set<String> operationTypes;
-  
   private final Optional<String> subprotocol;
-  
+  private Optional<String> methodName;
+
   private Form(String href, Optional<String> methodName, String mediaType, Set<String> operationTypes,
-      Optional<String> subprotocol) {
+               Optional<String> subprotocol) {
     this.methodName = methodName;
     this.target = href;
     this.contentType = mediaType;
@@ -35,23 +39,36 @@ public class Form {
   public Optional<String> getMethodName() {
     return methodName;
   }
-  
+
+  // Package-level access, used for setting affordance-specific default values after instantiation
+  void setMethodName(String methodName) {
+    this.methodName = Optional.of(methodName);
+  }
+
   public Optional<String> getMethodName(String operationType) {
     if (!operationTypes.contains(operationType)) {
       throw new IllegalArgumentException("Unknown operation type: " + operationType);
     }
-    
+
     if (methodName.isPresent()) {
       return methodName;
     }
-    
-    if (DEFAULT_HTTP_BINDING.containsKey(operationType)) {
-      return Optional.of(DEFAULT_HTTP_BINDING.get(operationType));
+
+    if (target.contains("http:") || target.contains("https:")){
+      if (DEFAULT_BINDING.containsKey("HTTP", operationType)) {
+        return Optional.of((String) DEFAULT_BINDING.get("HTTP", operationType));
+      }
     }
-    
+
+    if (target.contains("coap:") || target.contains("coaps:")){
+      if (DEFAULT_BINDING.containsKey("COAP", operationType)) {
+        return Optional.of((String) DEFAULT_BINDING.get("COAP", operationType));
+      }
+    }
+
     return Optional.empty();
   }
-  
+
   public String getTarget() {
     return target;
   }
@@ -59,37 +76,41 @@ public class Form {
   public String getContentType() {
     return contentType;
   }
-  
+
   public boolean hasOperationType(String type) {
     return operationTypes.contains(type);
   }
-  
+
   public Set<String> getOperationTypes() {
     return operationTypes;
   }
-  
+
   public Optional<String> getSubProtocol() {
-    return this.subprotocol;
+    if (subprotocol.isPresent()) {
+      return subprotocol;
+    }
+
+    if (operationTypes.contains(TD.observeProperty)){
+      if (target.startsWith("<coap:") || target.startsWith("<coaps:")){
+        return Optional.of(COV.observe);
+      }
+    }
+
+    return Optional.empty();
   }
-  
-  // Package-level access, used for setting affordance-specific default values after instantiation
-  void setMethodName(String methodName) {
-    this.methodName = Optional.of(methodName);
-  }
-  
+
   // Package-level access, used for setting affordance-specific default values after instantiation
   void addOperationType(String operationType) {
     this.operationTypes.add(operationType);
   }
-  
+
   public static class Builder {
     private final String target;
+    private final Set<String> operationTypes;
     private Optional<String> methodName;
     private String contentType;
-    private final Set<String> operationTypes;
-    
     private Optional<String> subprotocol;
-    
+
     public Builder(String target) {
       this.target = target;
       this.methodName = Optional.empty();
@@ -97,37 +118,37 @@ public class Form {
       this.operationTypes = new HashSet<String>();
       this.subprotocol = Optional.empty();
     }
-    
+
     public Builder addOperationType(String operationType) {
       this.operationTypes.add(operationType);
       return this;
     }
-    
+
     public Builder addOperationTypes(Set<String> operationTypes) {
       this.operationTypes.addAll(operationTypes);
       return this;
     }
-    
+
     public Builder setMethodName(String methodName) {
       this.methodName = Optional.of(methodName);
       return this;
     }
-    
+
     public Builder setContentType(String contentType) {
       this.contentType = contentType;
       return this;
     }
-    
+
     public Builder addSubProtocol(String subprotocol) {
       this.subprotocol = Optional.of(subprotocol);
       return this;
     }
-    
+
     public Form build() {
-      return new Form(this.target, this.methodName, this.contentType, this.operationTypes, 
-          this.subprotocol);
+      return new Form(this.target, this.methodName, this.contentType, this.operationTypes,
+        this.subprotocol);
     }
-    
+
   }
-  
+
 }
