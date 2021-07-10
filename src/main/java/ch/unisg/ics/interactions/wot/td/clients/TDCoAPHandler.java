73e20d1df879dadb61892a686e98df2a0e50dd6a
danaivach <danai.vacht@gmail.com>
2021-07-10 17:27:30 +0200

    Update handlers

--- src/main/java/ch/unisg/ics/interactions/wot/td/clients/TDCoAPHandler.java
+++ src/main/java/ch/unisg/ics/interactions/wot/td/clients/TDCoAPHandler.java
@@ -2,24 +2,38 @@ package ch.unisg.ics.interactions.wot.td.clients;
 
 import org.eclipse.californium.core.CoapHandler;
 import org.eclipse.californium.core.CoapResponse;
-
+/**
+ * An abstract class for reacting to responses from
+ * asynchronous CoAP requests. A concrete <code>TDCoAPHandler</code>
+ * instance needs to be used when using the methods
+ * {@link TDCoapRequest#execute(TDCoAPHandler)} and
+ * {@link TDCoapRequest#establishRelation(TDCoAPHandler)}.
+ */
 public abstract class TDCoAPHandler {
 
   private final CoapHandler coapHandler = new CoapHandler() {
     @Override
     public void onLoad(CoapResponse response) {
-      onResponse(new TDCoapResponse(response));
+      handleLoad(new TDCoapResponse(response));
     }
 
     @Override
     public void onError() {
-      onFail();
+      handleError();
     }
   };
 
-  public abstract void onResponse(TDCoapResponse response);
+  /**
+   * Invoked when a CoAP response or notification has arrived.
+   *
+   * @param response the response
+   */
+  public abstract void handleLoad(TDCoapResponse response);
 
-  public abstract void onFail();
+  /**
+   * Invoked when a request timeouts or has been rejected by the server.
+   */
+  public abstract void handleError();
 
   CoapHandler getCoapHandler(){
     return this.coapHandler;
