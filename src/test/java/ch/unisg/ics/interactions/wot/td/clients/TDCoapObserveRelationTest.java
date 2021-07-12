0c33a0cbfea9259ad2851dc7f64cbfbf27505333
danaivach <danai.vacht@gmail.com>
2021-07-12 23:19:12 +0200

    Test all methods with mock

--- src/test/java/ch/unisg/ics/interactions/wot/td/clients/TDCoapObserveRelationTest.java
+++ src/test/java/ch/unisg/ics/interactions/wot/td/clients/TDCoapObserveRelationTest.java
@@ -5,15 +5,18 @@ import org.eclipse.californium.core.coap.CoAP;
 import org.eclipse.californium.core.coap.Response;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.stubbing.Answer;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.*;
 
 public class TDCoapObserveRelationTest {
 
   private final CoapObserveRelation relationMock = mock(CoapObserveRelation.class);
   private final TDCoapObserveRelation relation = new TDCoapObserveRelation(relationMock);
+
+  private String mockHolder = "no_invocation";
 
   @Before
   public void init() {
@@ -22,6 +25,21 @@ public class TDCoapObserveRelationTest {
 
     when(relationMock.getCurrentResponse()).thenReturn(response);
     when(relationMock.isCanceled()).thenReturn(false);
+
+    doAnswer((Answer<Void>) invocation -> {
+      mockHolder = "reactive_cancel";
+      return null;
+    }).when(relationMock).reactiveCancel();
+
+    doAnswer((Answer<Void>) invocation -> {
+      mockHolder = "proactive_cancel";
+      return null;
+    }).when(relationMock).proactiveCancel();
+
+    doAnswer((Answer<Void>) invocation -> {
+      mockHolder = "reregister";
+      return null;
+    }).when(relationMock).reregister();
   }
 
   @Test
@@ -33,5 +51,26 @@ public class TDCoapObserveRelationTest {
   @Test
   public void testIsCanceled() {
     assertEquals(false, relation.isCanceled());
+  }
+
+  @Test
+  public void testReactiveCancel() {
+    assertNotEquals("reactive_cancel", mockHolder);
+    relation.reactiveCancel();
+    assertEquals("reactive_cancel", mockHolder);
+  }
+
+  @Test
+  public void testProactiveCancel() {
+    assertNotEquals("proactive_cancel", mockHolder);
+    relation.proactiveCancel();
+    assertEquals("proactive_cancel", mockHolder);
+  }
+
+  @Test
+  public void testReregister() {
+    assertNotEquals("reregister", mockHolder);
+    relation.reregister();
+    assertEquals("reregister", mockHolder);
   }
 }
