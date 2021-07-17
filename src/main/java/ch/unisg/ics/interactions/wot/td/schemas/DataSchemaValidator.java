fd9adf8641d2d62e1531f43f0f493cac45cb1df3
danaivach <danai.vacht@gmail.com>
2021-07-17 16:16:36 +0200

    Validate NullSchema

--- src/main/java/ch/unisg/ics/interactions/wot/td/schemas/DataSchemaValidator.java
+++ src/main/java/ch/unisg/ics/interactions/wot/td/schemas/DataSchemaValidator.java
@@ -1,4 +1,31 @@
 package ch.unisg.ics.interactions.wot.td.schemas;
 
+import java.util.Objects;
+
 public class DataSchemaValidator {
+
+  public static boolean validate(DataSchema schema, Object value) {
+    switch(schema.getDatatype()) {
+      case DataSchema.STRING:
+        return false;
+      case DataSchema.NUMBER:
+        return false;
+      case DataSchema.INTEGER:
+        return false;
+      case DataSchema.BOOLEAN:
+        return false;
+      case DataSchema.ARRAY:
+        return false;
+      case DataSchema.OBJECT:
+        return false;
+      case DataSchema.NULL:
+        if (!Objects.isNull(value)) {
+          return false;
+        }
+        break;
+      default:
+        break;
+    }
+    return true;
+  }
 }
--- src/test/java/ch/unisg/ics/interactions/wot/td/schemas/DataSchemaValidatorTest.java
+++ src/test/java/ch/unisg/ics/interactions/wot/td/schemas/DataSchemaValidatorTest.java
@@ -1,4 +1,35 @@
 package ch.unisg.ics.interactions.wot.td.schemas;
 
+import org.apache.commons.collections4.map.HashedMap;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class DataSchemaValidatorTest {
+
+  @Test
+  public void testValidateNullSchema() {
+
+    NullSchema nullSchema = new NullSchema.Builder().build();
+
+    assertTrue(DataSchemaValidator.validate(nullSchema,null));
+
+    assertFalse(DataSchemaValidator.validate(nullSchema,"1"));
+    assertFalse(DataSchemaValidator.validate(nullSchema,1));
+    assertFalse(DataSchemaValidator.validate(nullSchema,1.5));
+    assertFalse(DataSchemaValidator.validate(nullSchema,true));
+
+    List<String> arrayValue = new ArrayList<>();
+    arrayValue.add("Rimuru");
+    assertFalse(DataSchemaValidator.validate(nullSchema,arrayValue));
+
+    Map<String,Object> objectValue = new HashedMap<>();
+    objectValue.put("name","Rimuru");
+    assertFalse(DataSchemaValidator.validate(nullSchema,objectValue));
+  }
 }
