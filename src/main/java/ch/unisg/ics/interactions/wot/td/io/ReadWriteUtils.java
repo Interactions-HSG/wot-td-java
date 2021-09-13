a0884f247e4da79639d90d749806b3bdc2198527
Samuele <samuele.bura98@gmail.com>
2021-09-13 15:16:32 +0200

    Add support for JSON-LD 1.1

--- src/main/java/ch/unisg/ics/interactions/wot/td/io/ReadWriteUtils.java
+++ src/main/java/ch/unisg/ics/interactions/wot/td/io/ReadWriteUtils.java
@@ -1,12 +1,16 @@
 package ch.unisg.ics.interactions.wot.td.io;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.StringReader;
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import ch.unisg.ics.interactions.wot.td.security.SecurityScheme;
+import com.apicatalog.jsonld.JsonLd;
+import com.apicatalog.jsonld.JsonLdError;
+import com.apicatalog.jsonld.document.Document;
+import com.apicatalog.jsonld.document.JsonDocument;
+
+import com.apicatalog.jsonld.document.RdfDocument;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.impl.LinkedHashModel;
 import org.eclipse.rdf4j.rio.RDFFormat;
@@ -21,24 +25,28 @@ import org.eclipse.rdf4j.rio.helpers.StatementCollector;
 final class ReadWriteUtils {
   private final static Logger LOGGER = Logger.getLogger(ReadWriteUtils.class.getCanonicalName());
 
-  static Model readModelFromString(RDFFormat format, String description, String baseURI) 
+  static Model readModelFromString(RDFFormat format, String description, String baseURI)
       throws RDFParseException, RDFHandlerException, IOException {
     StringReader stringReader = new StringReader(description);
-    
+
     RDFParser rdfParser = Rio.createParser(RDFFormat.TURTLE);
     Model model = new LinkedHashModel();
     rdfParser.setRDFHandler(new StatementCollector(model));
-    
+
     rdfParser.parse(stringReader, baseURI);
-    
+
     return model;
   }
-  
+
   static String writeToString(RDFFormat format, Model model) {
     OutputStream out = new ByteArrayOutputStream();
-    
+    //TODO: remove this when rdf4j starts supporting JSON-LD 1.1
+    if(format.getName().contains("JSON-LD")){
+      return writeToJsonLD(model);
+    }
+
     try {
-      Rio.write(model, out, format, 
+      Rio.write(model, out, format,
           new WriterConfig().set(BasicWriterSettings.INLINE_BLANK_NODES, true));
     } finally {
       try {
@@ -47,9 +55,21 @@ final class ReadWriteUtils {
         LOGGER.log(Level.WARNING, e.getMessage());
       }
     }
-    
+
     return out.toString();
   }
-  
+
+  //This is a hack - purposefully ugly just to make it work for the time being
+  static String writeToJsonLD(Model model) {
+    InputStream rdfStream = new ByteArrayInputStream(writeToString(RDFFormat.NTRIPLES, model).getBytes());
+    try {
+      Document document = RdfDocument.of(rdfStream);
+      return JsonLd.fromRdf(document).get().toString();
+    } catch (JsonLdError e) {
+      e.printStackTrace();
+    }
+    return "";
+  }
+
   private ReadWriteUtils() { }
 }
--- src/main/java/ch/unisg/ics/interactions/wot/td/io/TDGraphWriter.java
+++ src/main/java/ch/unisg/ics/interactions/wot/td/io/TDGraphWriter.java
@@ -29,29 +29,29 @@ import ch.unisg.ics.interactions.wot.td.vocabularies.TD;
 /**
  * A writer for serializing TDs as RDF graphs. Provides a fluent API for adding prefix bindings to be
  * used in the serialization.
- * 
+ *
  */
 public class TDGraphWriter {
   private final Resource thingId;
   private final ThingDescription td;
   private final ModelBuilder graphBuilder;
   private final ValueFactory rdf = SimpleValueFactory.getInstance();
-  
+
   public TDGraphWriter(ThingDescription td) {
     this.thingId = td.getThingURI().isPresent() ? rdf.createIRI(td.getThingURI().get())
         : rdf.createBNode();
-    
+
     this.td = td;
     this.graphBuilder = new ModelBuilder();
   }
-  
+
   public static String write(ThingDescription td) {
     return new TDGraphWriter(td).write();
   }
-  
+
   /**
    * Sets a prefix binding for a given namespace.
-   * 
+   *
    * @param prefix the prefix to be used in the serialized representation
    * @param namespace the given namespace
    * @return this <code>TDGraphWriter</code>
@@ -60,7 +60,7 @@ public class TDGraphWriter {
     this.graphBuilder.setNamespace(prefix, namespace);
     return this;
   }
-  
+
   public String write() {
     return this.addTypes()
         .addTitle()
@@ -69,139 +69,139 @@ public class TDGraphWriter {
         .addProperties()
         .addActions()
         .addGraph()
-        .write(RDFFormat.TURTLE);
+        .write(RDFFormat.JSONLD); //TODO change
   }
-  
+
   private Model getModel() {
     return graphBuilder.build();
   }
-  
+
   private TDGraphWriter addSecurity() {
     List<SecurityScheme> securitySchemes = td.getSecuritySchemes();
-    
+
     for (SecurityScheme scheme : securitySchemes) {
       BNode schemeId = rdf.createBNode();
       graphBuilder.add(thingId, rdf.createIRI(TD.hasSecurityConfiguration), schemeId);
-      
+
       Model schemeGraph = scheme.toRDF(schemeId);
       for (Statement s : schemeGraph) {
         graphBuilder.add(s.getSubject(), s.getPredicate(), s.getObject());
       }
     }
-    
+
     return this;
   }
-  
+
   private TDGraphWriter addTypes() {
     graphBuilder.add(thingId, RDF.TYPE, rdf.createIRI(TD.Thing));
-    
+
     for (String type : td.getSemanticTypes()) {
       graphBuilder.add(thingId, RDF.TYPE, rdf.createIRI(type));
     }
-    
+
     return this;
   }
-  
+
   private TDGraphWriter addTitle() {
     graphBuilder.add(thingId, rdf.createIRI(DCT.title), td.getTitle());
     return this;
   }
-    
+
   private TDGraphWriter addBaseURI() {
     if (td.getBaseURI().isPresent()) {
-      graphBuilder.add(thingId, rdf.createIRI(TD.hasBase), 
+      graphBuilder.add(thingId, rdf.createIRI(TD.hasBase),
           rdf.createIRI(td.getBaseURI().get()));
     }
-    
+
     return this;
   }
-  
+
   private TDGraphWriter addProperties() {
     for (PropertyAffordance property : td.getProperties()) {
       Resource propertyId = addAffordance(property, TD.hasPropertyAffordance, TD.PropertyAffordance);
       graphBuilder.add(propertyId, rdf.createIRI(TD.isObservable), property.isObservable());
-      
+
       SchemaGraphWriter.write(graphBuilder, propertyId, property.getDataSchema());
     }
-    
+
     return this;
   }
-  
+
   private TDGraphWriter addActions() {
     for (ActionAffordance action : td.getActions()) {
       Resource actionId = addAffordance(action, TD.hasActionAffordance, TD.ActionAffordance);
-      
+
       if (action.getInputSchema().isPresent()) {
         DataSchema schema = action.getInputSchema().get();
-        
+
         Resource inputId = rdf.createBNode();
         graphBuilder.add(actionId, rdf.createIRI(TD.hasInputSchema), inputId);
-        
+
         SchemaGraphWriter.write(graphBuilder, inputId, schema);
       }
-      
+
       if (action.getOutputSchema().isPresent()) {
         DataSchema schema = action.getOutputSchema().get();
-        
+
         Resource outputId = rdf.createBNode();
         graphBuilder.add(actionId, rdf.createIRI(TD.hasOutputSchema), outputId);
-        
+
         SchemaGraphWriter.write(graphBuilder, outputId, schema);
       }
     }
-    
+
     return this;
   }
-  
+
   private TDGraphWriter addGraph() {
   	if(td.getGraph().isPresent()) {
   		getModel().addAll(td.getGraph().get());
-  		
+
   		td.getGraph().get().getNamespaces().stream()
   		    .filter(ns -> !getModel().getNamespace(ns.getPrefix()).isPresent())
           .forEach(graphBuilder::setNamespace);
   	}
   	return this;
   }
-  
-  private Resource addAffordance(InteractionAffordance affordance, String affordanceProp, 
+
+  private Resource addAffordance(InteractionAffordance affordance, String affordanceProp,
       String affordanceClass) {
     BNode affordanceId = rdf.createBNode();
-    
+
     graphBuilder.add(thingId, rdf.createIRI(affordanceProp), affordanceId);
     graphBuilder.add(affordanceId, RDF.TYPE, rdf.createIRI(affordanceClass));
-    
+
     for (String type : affordance.getSemanticTypes()) {
       graphBuilder.add(affordanceId, RDF.TYPE, rdf.createIRI(type));
     }
-    
+
     if (affordance.getName().isPresent()) {
-      graphBuilder.add(affordanceId, rdf.createIRI(TD.name), 
+      graphBuilder.add(affordanceId, rdf.createIRI(TD.name),
           rdf.createLiteral(affordance.getName().get()));
     }
-    
+
     if (affordance.getTitle().isPresent()) {
       graphBuilder.add(affordanceId, rdf.createIRI(DCT.title), affordance.getTitle().get());
     }
-    
+
     addFormsForInteraction(affordanceId, affordance);
-    
+
     return affordanceId;
   }
-    
+
   private void addFormsForInteraction(Resource interactionId, InteractionAffordance interaction) {
     for (Form form : interaction.getForms()) {
       BNode formId = rdf.createBNode();
-      
+
       graphBuilder.add(interactionId, rdf.createIRI(TD.hasForm), formId);
-      
-      // Only writes the method name for forms with one operation type (to avoid ambiguity)  
+
+      // Only writes the method name for forms with one operation type (to avoid ambiguity)
       if (form.getMethodName().isPresent() && form.getOperationTypes().size() == 1) {
         graphBuilder.add(formId, rdf.createIRI(HTV.methodName), form.getMethodName().get());
       }
       graphBuilder.add(formId, rdf.createIRI(HCTL.hasTarget), rdf.createIRI(form.getTarget()));
       graphBuilder.add(formId, rdf.createIRI(HCTL.forContentType), form.getContentType());
-      
+
       for (String opType : form.getOperationTypes()) {
         try {
           IRI opTypeIri = rdf.createIRI(opType);
@@ -210,14 +210,14 @@ public class TDGraphWriter {
           graphBuilder.add(formId, rdf.createIRI(HCTL.hasOperationType), opType);
         }
       }
-      
+
       Optional<String> subprotocol = form.getSubProtocol();
       if (subprotocol.isPresent()) {
         graphBuilder.add(formId, rdf.createIRI(HCTL.forSubProtocol), subprotocol.get());
       }
     }
   }
-  
+
   private String write(RDFFormat format) {
     return ReadWriteUtils.writeToString(format, getModel());
   }
