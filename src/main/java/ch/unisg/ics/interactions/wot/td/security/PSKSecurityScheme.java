9ac5b6bc577edaf2e09ed435a1ce54a881a25656
danaivach <danai.vacht@gmail.com>
2022-02-13 21:54:47 +0100

    Fix identation

--- src/main/java/ch/unisg/ics/interactions/wot/td/security/PSKSecurityScheme.java
+++ src/main/java/ch/unisg/ics/interactions/wot/td/security/PSKSecurityScheme.java
@@ -8,47 +8,47 @@ import java.util.Set;
 
 public class PSKSecurityScheme extends SecurityScheme {
 
-    private final Optional<String> identity;
+  private final Optional<String> identity;
 
-    protected PSKSecurityScheme(Map<String, Object> configuration, Set<String> semanticTypes, Optional<String> identity) {
-        super(SecurityScheme.PSK, configuration, semanticTypes);
-        this.identity = identity;
+  protected PSKSecurityScheme(Map<String, Object> configuration, Set<String> semanticTypes, Optional<String> identity) {
+    super(SecurityScheme.PSK, configuration, semanticTypes);
+    this.identity = identity;
+  }
+
+  /**
+   * Gets the identifier providing information which can be used for selection or confirmation.
+   *
+   * @return the quality of protection
+   */
+  public Optional<String> getIdentity() {
+    return identity;
+  }
+
+  public static class Builder extends SecurityScheme.Builder<PSKSecurityScheme,
+    PSKSecurityScheme.Builder> {
+
+    private Optional<String> identity;
+
+    public Builder() {
+      this.identity = Optional.empty();
+      this.semanticTypes.add(WoTSec.PSKSecurityScheme);
     }
 
     /**
-     * Gets the identifier providing information which can be used for selection or confirmation.
+     * Specifies the identifier providing information which can be used for selection or confirmation.
      *
-     * @return the quality of protection
+     * @param identity the identifier
+     * @return the builder
      */
-    public Optional<String> getIdentity() {
-        return identity;
+    public PSKSecurityScheme.Builder addIdentity(String identity) {
+      this.identity = Optional.of(identity);
+      this.configuration.put(WoTSec.identity, identity);
+      return this;
     }
 
-    public static class Builder extends SecurityScheme.Builder<PSKSecurityScheme,
-            PSKSecurityScheme.Builder> {
-
-        private Optional<String> identity;
-
-        public Builder() {
-            this.identity = Optional.empty();
-            this.semanticTypes.add(WoTSec.PSKSecurityScheme);
-        }
-
-        /**
-         * Specifies the identifier providing information which can be used for selection or confirmation.
-         *
-         * @param identity the identifier
-         * @return the builder
-         */
-        public PSKSecurityScheme.Builder addIdentity(String identity) {
-            this.identity = Optional.of(identity);
-            this.configuration.put(WoTSec.identity, identity);
-            return this;
-        }
-
-        @Override
-        public PSKSecurityScheme build() {
-            return new PSKSecurityScheme(configuration, semanticTypes, identity);
-        }
+    @Override
+    public PSKSecurityScheme build() {
+      return new PSKSecurityScheme(configuration, semanticTypes, identity);
     }
+  }
 }
